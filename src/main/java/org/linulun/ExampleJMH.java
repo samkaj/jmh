a994294ccb5c1e06137dd5aae74b48be610869e9
samkaj <kajavasamuel@gmail.com>
2023-03-02 10:19:27 +0100

    initial commit

---
+++ src/main/java/org/linulun/ExampleJMH.java
@@ -0,0 +1,19 @@
+package org.linulun;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+
+public class ExampleJMH {
+
+    @Benchmark
+    @BenchmarkMode(Mode.SingleShotTime)
+    @OutputTimeUnit(TimeUnit.MICROSECONDS)
+    public void measureSingleShot() throws InterruptedException {
+        TimeUnit.MILLISECONDS.sleep(100);
+    }
+
+}
